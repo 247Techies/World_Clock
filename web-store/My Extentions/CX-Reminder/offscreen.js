29dc1c712f63c7ab65075537dff91c4f5a883706
247Techies <aidentechie@gmail.com>
2025-07-11 05:56:17 +0530

    Delete web-store/My Extentions directory

--- web-store/My Extentions/CX-Reminder/offscreen.js
+++
@@ -1,8 +0,0 @@
-// This script runs in the offscreen document.
-
-chrome.runtime.onMessage.addListener((msg) => {
-    if (msg.type === 'play-sound') {
-      const audio = new Audio(msg.file);
-      audio.play();
-    }
-  });
--- web-store/My Extentions/CX-Renew-VM/background.js
+++
@@ -1,8 +0,0 @@
-// Listen for when the user clicks the extension's browser action icon
-chrome.action.onClicked.addListener((tab) => {
-    // Inject the content.js script into the currently active tab
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['content.js']
-    });
-  });
